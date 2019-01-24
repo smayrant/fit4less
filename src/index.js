4e967f1fc88d3015b48ca0945641c12d7bb7a666
Sheldrick <mrmayrant@yahoo.com>
2019-01-24 09:07:38 -0500

    first commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+require('./styles/index.scss');
+require('./styles/header.scss');
+
+import PopperJs from 'popper.js';
+import jquery from 'jquery';
+
+
+jquery(() => {
+    console.log('Hello jQuery + bootstrap 4!');
+});
