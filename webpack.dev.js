3cf85d6eec18829334720d9092a8dfb7e347debb
Sheldrick <mrmayrant@yahoo.com>
2019-01-31 01:33:46 -0500

    correctly added the class images for the class section

--- webpack.dev.js
+++ webpack.dev.js
@@ -54,7 +54,7 @@ module.exports = {
             ,
             {
                 // Load all images as base64 encoding if they are smaller than 8192 bytes
-                test: /\.(png|jpg|gif)$/,
+                test: /\.(png|jpg|gif|jpeg)$/,
                 use: [
                     {
                         loader: 'url-loader',
